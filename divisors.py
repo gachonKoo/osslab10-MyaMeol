7d5f2779f75d6f645ee25a4dab8eb448e2771e00
MyaMeol <akvga@naver.com>
2025-11-09 18:28:50 +0900

    Add divisors.py

--- divisors.py
+++ divisors.py
@@ -1,20 +1,9 @@
 import sys
 
 def get_divisors(n):
-    divisors = []
-    for i in range(1, n + 1):
-        if n % i == 0:
-            divisors.append(i)
+    divisors = [i for i in range(1, n + 1) if n % i == 0]
     return divisors
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python divisors.py <number>")
-        sys.exit(1)
-
-    try:
-        n = int(sys.argv[1])
-        result = get_divisors(n)
-        print(" ".join(str(x) for x in result))
-    except Exception as e:
-        print("Error:", e)
+    n = int(sys.argv[1])
+    print(" ".join(map(str, get_divisors(n))))
